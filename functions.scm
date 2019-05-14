98ca1f0c8d79a23aa17c0071d5714a177d3eaa98
Joshua Tate <jmtate@icloud.com>
2019-05-14 01:11:48 -0700

    Added notes from lecture on Monday May 13

--- functions.scm
+++ functions.scm
@@ -39,3 +39,69 @@
     (cond ((< n 0) 'negative)
 	  ((= n 0) 'zero)
 	  (else 'positive) )))
+
+;; Use recursion instead of loops
+(define len
+  (lambda (lst)
+    (cond ((null? lst) 0)
+	  (else (+ 1 (len (cdr lst)))) )))
+
+
+(define contains
+  (lambda (x lst)
+    (cond ((null? lst) #f)
+	  ((equal? x (car lst)) #t)
+	  (else (contains x (cdr lst))) )))
+
+(define count-syml
+  (lambda (lst)
+    (cond ((null? lst) 0)
+	  ((symbol? (car lst)) (+ 1 (count-syml (cdr lst))))
+	  (else (count-syml (cdr lst))) )))
+
+
+;; (if #t 'a 'b) ==> returns a
+;; (if #f 'a 'b) ==> returns b
+(define count-sym2
+  (lambda (lst)
+    (if (null? lst)
+	0
+	(+ (if (symbol? (car lst)) 1 0)
+	   (count-sym2 (cdr lst)) ))))
+
+
+(define count-num
+  (lambda (lst)
+    (cond ((null? lst) 0)
+	  ((number? (car lst)) (+ 1 (count-num (cdr lst))))
+	  (else (count-num (cdr lst))) )))
+
+;; Can use ? in variable names
+(define count-fn
+  (lambda (pred? lst)
+    (cond ((null? lst) 0)
+	  ((pred? (car lst)) (+ 1 (count-fn pred? (cdr lst))))
+	  (else (count-fn pred? (cdr lst))) )))
+
+(define contains-fn
+  (lambda (pred? lst)
+    (cond ((null? lst) #f)
+	  ((pred? (car lst)) #t)
+	  (else (contains-fn pred? (cdr lst))) )))
+
+(define contains
+  (lambda (x lst)
+    (contains-fn (lambda (a) (equal? a x)) lst) ))
+
+;; (flatten '(1 2 (a b) (((3))) 4)) => (1 2 a b 3 4)
+;; implementation is not very efficient, but very useful
+
+;; May need to implement append on exam!!
+;; NOTE: Not allowed to use append in assignment 1
+(define flatten
+  (lambda (x)
+    (cond ((not (list? x)) x)
+	  ((null? x) x)
+	  ((list? (car x)) (append (flatten (car x))
+				   (flatten (cdr x))))
+	  (else (cons (car x) (flatten (cdr x)))) )))
