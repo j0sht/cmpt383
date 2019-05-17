938b687c6adf29ee35f648924f054ea6b94723ff
Joshua Tate <jmtate@icloud.com>
2019-05-17 01:06:15 -0700

    Implemented problems 2 and 3

--- assignments/a1/a1.scm
+++ assignments/a1/a1.scm
@@ -2,3 +2,15 @@
   (lambda (x)
     (and (list? x)
 	 (equal? (cdr x) '()) )))
+
+(define my-make-list
+  (lambda (n x)
+    (if (equal? n 0) '()
+	(cons x (my-make-list (- n 1) x)) )))
+
+(define all-same?
+  (lambda (lst)
+    (cond ((null? lst) #t)
+	  (else (and (or (null? (cdr lst))
+			 (equal? (car lst) (car (cdr lst))))
+		     (all-same? (cdr lst)) )))))
