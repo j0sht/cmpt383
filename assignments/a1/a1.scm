ea956421d019d9c7c0edf62032ae610a7f092ab1
Joshua Tate <jmtate@icloud.com>
2019-05-17 13:06:35 -0700

    Implemented problems 6 and 7

--- assignments/a1/a1.scm
+++ assignments/a1/a1.scm
@@ -5,7 +5,8 @@
 
 (define my-make-list
   (lambda (n x)
-    (if (equal? n 0) '()
+    (if (equal? n 0)
+	'()
 	(cons x (my-make-list (- n 1) x)) )))
 
 (define all-same?
@@ -20,11 +21,29 @@
     (let ((max n))
       (define inner-iota
 	(lambda (n)
-	  (if (equal? n 0) '()
+	  (if (equal? n 0)
+	      '()
 	      (cons (- max n) (inner-iota (- n 1))))))
       (inner-iota n) )))
 
 (define my-length
   (lambda (lst)
-    (if (null? lst) 0
+    (if (null? lst)
+	0
 	(+ 1 (my-length (cdr lst))) )))
+
+(define nth
+  (lambda (lst i)
+    (if (and (not (null? lst)) (equal? i 0))
+	(car lst)
+	(if (or (null? lst) (< i 0))
+	    (error "bad index")
+	    (nth (cdr lst) (- i 1)) ))))
+
+(define my-last
+  (lambda (lst)
+    (if (and (not (null? lst)) (null? (cdr lst)))
+	(car lst)
+	(if (null? lst)
+	    (error "my-last: empty list")
+	    (my-last (cdr lst)) ))))
